80e4b3fa947084a9c3da2974c689f46093e022a6
fakdooandroid01 <59410854+fakdooandroid01@users.noreply.github.com>
2020-01-03 16:50:21 +0530

    activity_login.xml activity_login_with.email.xml activity_login_with_mobile.xml activity_splash.xml

---
+++ app/src/main/java/com/application/fakdoo/login_with_mobile.java
@@ -0,0 +1,14 @@
+package com.application.fakdoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_with_mobile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_with_mobile);
+    }
+}
