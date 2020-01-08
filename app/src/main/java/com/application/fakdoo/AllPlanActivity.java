4bbfffc21aee501bd11e52d03aa62501b8f84d3f
fakdooandroid01 <59410854+fakdooandroid01@users.noreply.github.com>
2020-01-08 16:35:48 +0530

    xml design

---
+++ app/src/main/java/com/application/fakdoo/AllPlanActivity.java
@@ -0,0 +1,14 @@
+package com.application.fakdoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllPlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_plan);
+    }
+}
---
+++ app/src/main/java/com/application/fakdoo/MyPlanActivity.java
@@ -0,0 +1,14 @@
+package com.application.fakdoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyPlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_plan);
+    }
+}
